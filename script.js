712c8cfeb70a277c8021547110a1056e78f64345
fabioferrero90 <fabio.ff90@gmail.com>
2024-11-18 15:32:42 +0100

    fix: code refactor without using includes() method

--- script.js
+++ script.js
@@ -22,43 +22,44 @@ let isAllowed;
 //Inizio il ciclo
 while (!isAllowed) {
   // Mostro un prompt dove inserire la mail
-  let userEmail = prompt("Inserisci il tuo indirizzo mail");
+  let promptedEmail = prompt("Inserisci il tuo indirizzo mail");
 
-  // Controllo che la mail inserita sia nella lista (BONUS: e che sia una mail), e se si, procedo con il gioco dei dadi
-  if (userEmail && allowedEmails.includes(userEmail.toLowerCase()) && userEmail.includes('@')) {
-    alert("La tua mail ha accesso a questo gioco!, premi OK per lanciare i dadi");
-    isAllowed = true;
+  // Controllo che nel prompt sia stata inserita una mail
+  if (promptedEmail && promptedEmail.includes('@') && promptedEmail.includes('.')){
 
-    console.log("Inizio il gioco dei dadi");
+      // Controllo che la mail inserita sia nella lista (BONUS: e che sia una mail), e se si, procedo con il gioco dei dadi
+    for( let i = 0 ; i < allowedEmails.length; i++) {
+      if (promptedEmail.toLowerCase() == allowedEmails[i]){
+        isAllowed = true;
+        alert("La tua mail ha accesso a questo gioco, premi OK per lanciare i dadi!");
 
-    /* SECONDA PARTE - DICE GAME
+        /* SECONDA PARTE - DICE GAME
 
-      1.Genera due numeri random da 1 a 6, uno per il giocatore e uno per il computer
-      2.Stabilisci il vincitore in base a chi fa il punteggio più alto
+          1.Genera due numeri random da 1 a 6, uno per il giocatore e uno per il computer
+          2.Stabilisci il vincitore in base a chi fa il punteggio più alto
 
-    */
+        */
 
-    //Lancio i dadi
-    const playerDice = Math.ceil(Math.random()*6);
-    const computerDice = Math.ceil(Math.random()*6);
+        //Lancio i dadi
+        const playerDice = Math.ceil(Math.random()*6);
+        const computerDice = Math.ceil(Math.random()*6);
 
-    //Stabilisco il vincitore
-    let winner = "Il Computer ha vinto!"
-    if (playerDice == computerDice) {
-      winner = "Nessuno dei due ha vinto";
-    } else if (playerDice > computerDice){
-      winner = "Hai vinto contro il computer!";
+        //Stabilisco il vincitore
+        let winner = "Il Computer ha vinto!"
+        if (playerDice == computerDice) {
+          winner = "Nessuno dei due ha vinto";
+        } else if (playerDice > computerDice){
+          winner = "Hai vinto contro il computer!";
+        }
+
+        //Invio il messaggio
+        let message = `Il tuo numero è: ${playerDice}\nIl numero del Computer è: ${computerDice}\n\n${winner}`;
+        alert(message);
+      }
     }
-
-    //Invio il messaggio
-    let message = `Il tuo numero è: ${playerDice}\nIl numero del Computer è: ${computerDice}\n\n${winner}`;
-    alert(message);
-
-    //Se la mail non è stata inserita o non è una mail
-  } else if (!userEmail || !userEmail.includes('@')){
+  } else if (!promptedEmail || !(promptedEmail.includes('@') && promptedEmail.includes('.'))){
     alert("Devi inserire una mail valida per giocare!");
-    //Se la mail non è all'interno delle mail consentite
   } else {
-    alert("La tua mail non ha accesso a questo gioco!");
+    alert("Questa non è una mail valida per giocare!");
   }
 }
